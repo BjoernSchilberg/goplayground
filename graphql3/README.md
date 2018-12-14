becde3e0cd541fe05d11a73486af1bc1a5cb5a9f
Bjoern Schilberg <bjoern@intevation.de>
2018-12-14 14:47:12 +0100

    Added an more generic search field example.

---
+++ graphql3/README.md
@@ -0,0 +1,14 @@
+# README
+
+## Requirements
+
+```shell
+go get -u github.com/graphql-go/graphql
+```
+
+## Example query
+
+```shell
+curl -g 'http://localhost:3000/graphql?query={pirates{name,wikipedia}}'
+curl -g 'http://localhost:3000/graphql?query={pirates(country:"China"){name,wikipedia}}'
+```
---
+++ graphql3/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/fatih/structs"
+	"github.com/graphql-go/graphql"
+)
+
+func Filter(data []Pirate, args map[string]interface{}) []Pirate {
+	vsf := make([]Pirate, 0)
+	for _, p := range data {
+		for k, v := range args {
+			structMap := structs.Map(p)
+			if structMap[strings.Title(k)] != v {
+				break
+			}
+			vsf = append(vsf, p)
+		}
+
+	}
+	return vsf
+}
+
+func main() {
+
+	// GraphQL model
+	pirateType := graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Pirate",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.String,
+				},
+				"name": &graphql.Field{
+					Type: graphql.String,
+				},
+				"life": &graphql.Field{
+					Type: graphql.String,
+				},
+				"yearsactive": &graphql.Field{
+					Type: graphql.String,
+				},
+				"country": &graphql.Field{
+					Type: graphql.String,
+				},
+				"comments": &graphql.Field{
+					Type: graphql.String,
+				},
+				"wikipedia": &graphql.Field{
+					Type: graphql.String,
+				},
+			},
+		})
+
+	rootQuery := graphql.NewObject(
+
+		graphql.ObjectConfig{
+			Name: "Query",
+			Fields: graphql.Fields{
+				"pirates": &graphql.Field{
+					Type: graphql.NewList(pirateType),
+					Args: graphql.FieldConfigArgument{
+						"id": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+						"name": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+						"life": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+						"yearsactive": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+						"country": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+						"comments": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+					},
+					Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+						return Filter(pirates, params.Args), nil
+					},
+				},
+			},
+		})
+
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: r.URL.Query().Get("query"),
+		})
+		json.NewEncoder(w).Encode(result)
+	})
+	http.ListenAndServe(":3000", nil)
+}
